57e07811e2f3f46861582da3b50e6e416ecbd731
k3udara <kaushaleeudara@gmail.com>
2023-03-26 21:42:02 +0530

    code edited

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 
+from starlette.middleware.cors import CORSMiddleware
 from TextPreprocessor import TextAnalyzer
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 class userRequest(BaseModel):
     searchQuery : str
     paraArray: list
 
-@app.post('/summarizer',response_model= str)
+@app.post('/summarizer')
 async def getUserRequest(userR : userRequest):
     mlmodel = TextAnalyzer()
-    summ = mlmodel.runnerClass(userR.paraArray,userR.searchQuery)
+    summ =  mlmodel.runnerClass(userR.paraArray,userR.searchQuery)
     return summ
